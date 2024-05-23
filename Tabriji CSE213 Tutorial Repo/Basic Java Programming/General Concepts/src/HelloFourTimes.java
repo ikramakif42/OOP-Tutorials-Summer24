9975e99072e449592f579b188c8d8bdcaac625bc
Tabriji <tabrijiummi@gmail.com>
2024-05-23 19:30:43 +0600

    tutorial day 1

--- Tabriji CSE213 Tutorial Repo/Basic Java Programming/General Concepts/src/HelloFourTimes.java
+++ Tabriji CSE213 Tutorial Repo/Basic Java Programming/General Concepts/src/HelloFourTimes.java
@@ -1,7 +1,7 @@
 public class HelloFourTimes {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); // use control-d to copy lines faster
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
--- Tabriji CSE213 Tutorial Repo/Basic Java Programming/General Concepts/src/tabriji/UserInput.java
+++ Tabriji CSE213 Tutorial Repo/Basic Java Programming/General Concepts/src/tabriji/UserInput.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 public class UserInput {
     public static void main(String[] args) {
 
-        Scanner input_nibe_j = new Scanner(System.in); //initialization
+        Scanner input = new Scanner(System.in); //initialization
 
         System.out.print("Enter a number: ");
-        int a = input_nibe_j.nextInt();
+        int a = input.nextInt();
         System.out.print("Enter another number: ");
-        int b = input_nibe_j.nextInt();
-
+        int b = input.nextInt();
+// single line comment
+    /*
+        multi
+                line
+                        comment
+                                */
         int sum = a + b;
 
         System.out.println("Sum: " + sum);
