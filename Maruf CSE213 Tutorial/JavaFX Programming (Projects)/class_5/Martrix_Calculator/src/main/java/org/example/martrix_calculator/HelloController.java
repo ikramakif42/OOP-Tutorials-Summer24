6b211c6a0edfd4805c692f4656042ab1367ac4f7
Md Maruf <129549328+Maruf195@users.noreply.github.com>
2024-06-11 01:49:40 +0600

    n

--- Maruf CSE213 Tutorial/JavaFX Programming (Projects)/class_5/Martrix_Calculator/src/main/java/org/example/martrix_calculator/HelloController.java
+++ Maruf CSE213 Tutorial/JavaFX Programming (Projects)/class_5/Martrix_Calculator/src/main/java/org/example/martrix_calculator/HelloController.java
@@ -4,11 +4,62 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
     @FXML
-    private Label welcomeText;
+    private TextField matrixA10;
+    @FXML
+    private TextField result10;
+    @FXML
+    private TextField result00;
+    @FXML
+    private TextField result11;
+    @FXML
+    private TextField result01;
+    @FXML
+    private TextField matrixB01;
+    @FXML
+    private TextField matrixA01;
+    @FXML
+    private TextField matrixB00;
+    @FXML
+    private TextField matrixB11;
+    @FXML
+    private TextField matrixA00;
+    @FXML
+    private TextField matrixA11;
+    @FXML
+    private TextField matrixB10;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void handleInverseA(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleClear(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleMultiply(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleDeterminantA(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleTransposeA(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleAdd(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleSubtract(ActionEvent actionEvent) {
     }
 }
